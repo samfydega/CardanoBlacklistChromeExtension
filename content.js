245c64403e2934a1a23326449a3b62fc987f416a
Samuel Crombie <55171394+samfydega@users.noreply.github.com>
2021-12-15 20:03:17 -0500

    Delete content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(gotMessage);
-
-function gotMessage(message, sender, sendResponse) {
-    console.log(message);
-    if (message === "hello") {
-        console.log("EPIC");
-    }
-}
